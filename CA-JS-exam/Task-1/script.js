248aa4423c9f4ae5468495be3a95550dde7e8b79
tdna <tdna@lan12.belaidis.net>
2022-10-27 19:06:20 +0300

    Completed Task-1

--- CA-JS-exam/Task-1/script.js
+++ CA-JS-exam/Task-1/script.js
@@ -8,3 +8,32 @@ pamatyti jo pateikto svorio kovertavimą į:
 Pastaba: atvaizdavimas turi būti matomas pateikus formą ir pateikiamas
 <div id="output"></div> viduje, bei turi turėti bent minimalų stilių;
 ------------------------------------------------------------------- */
+
+const form = document.querySelector("form");
+
+const weightConverter = (event) => {
+  event.preventDefault();
+
+  const userInputValue = +document.querySelector("#search").value;
+  const output = document.querySelector("#output");
+
+  const calculateWeightInPounds = userInputValue * 2.2046;
+  const calculateWeightInGrams = userInputValue / 0.001;
+  const calculateWeightInOz = userInputValue * 35.274;
+
+  const poundsElement = document.createElement("p");
+  const gramsElement = document.createElement("p");
+  const ozElement = document.createElement("p");
+
+  if (userInputValue) {
+    poundsElement.textContent = `Your weight in Pounds is: ${calculateWeightInPounds}`;
+    gramsElement.textContent = `Your weight in Grams is: ${calculateWeightInGrams}`;
+    ozElement.textContent = `Your weight in Oz is: ${calculateWeightInOz}`;
+
+    output.append(poundsElement, gramsElement, ozElement);
+  } else {
+    alert("Please input only numbers");
+  }
+};
+
+form.addEventListener("submit", weightConverter);
