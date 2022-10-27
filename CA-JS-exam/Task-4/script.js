06669c7fb95d37dcc36e307d4cfa71efd7cb8bc9
tdna <tdna@lan12.belaidis.net>
2022-10-27 22:29:42 +0300

    Allmost completed Task-4

--- CA-JS-exam/Task-4/script.js
+++ CA-JS-exam/Task-4/script.js
@@ -9,4 +9,47 @@ Pastaba: Sukurta kortelė, kurioje yra informacija apie automobilį (brand), tur
 turėti bent minimalų stilių ir būti responsive;
 -------------------------------------------------------------------------- */
 
-const ENDPOINT = 'cars.json';
+const ENDPOINT = "cars.json";
+
+const getCars = async () => {
+  try {
+    const response = await fetch(ENDPOINT);
+    const cars = await response.json();
+
+    return cars;
+  } catch (error) {
+    console.error(error);
+  }
+};
+
+const createCarBrandCard = (car) => {
+  const carBrandContainer = document.createElement("div");
+  carBrandContainer.setAttribute("class", "carBrandContainer");
+
+  const carBrandElement = document.createElement("h2");
+  carBrandElement.setAttribute("class", "carBrand");
+  carBrandElement.innerText = car.brand;
+
+  carBrandContainer.append(carBrandElement);
+
+  const models = car.models.sort((a, b) => a.localeCompare(b));
+
+  models.forEach((model) => {
+    const carModelsElement = document.createElement("p");
+    carModelsElement.setAttribute("class", "carModels");
+
+    carModelsElement.innerText = model;
+    carBrandContainer.append(carModelsElement);
+  });
+
+  document.querySelector("#output").append(carBrandContainer);
+};
+
+const renderCarBrandCard = async () => {
+  document.querySelector("#output").replaceChildren();
+  const cars = await getCars();
+
+  cars.forEach((car) => createCarBrandCard(car));
+};
+
+await renderCarBrandCard();
