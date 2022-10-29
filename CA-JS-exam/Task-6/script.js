28d6daf6c94d96fc5adec810086434d8c9e4a40f
andriuszaz <a.zazeckas@gmail.com>
2022-10-29 13:36:05 +0300

    Completed 'array reduce and map' Task-6

--- CA-JS-exam/Task-6/script.js
+++ CA-JS-exam/Task-6/script.js
@@ -9,13 +9,24 @@ atveju grąžins visų "users" vardus naujame masyve pvz., ['John Smith', 'Ann S
 -------------------------------------------------------------------------- */
 
 const users = [
-  { id: '1', name: 'John Smith', age: 20 },
-  { id: '2', name: 'Ann Smith', age: 24 },
-  { id: '3', name: 'Tom Jones', age: 31 },
-  { id: '4', name: 'Rose Peterson', age: 17 },
-  { id: '5', name: 'Alex John', age: 25 },
-  { id: '6', name: 'Ronald Jones', age: 63 },
-  { id: '7', name: 'Elton Smith', age: 16 },
-  { id: '8', name: 'Simon Peterson', age: 30 },
-  { id: '9', name: 'Daniel Cane', age: 51 },
+  { id: "1", name: "John Smith", age: 20 },
+  { id: "2", name: "Ann Smith", age: 24 },
+  { id: "3", name: "Tom Jones", age: 31 },
+  { id: "4", name: "Rose Peterson", age: 17 },
+  { id: "5", name: "Alex John", age: 25 },
+  { id: "6", name: "Ronald Jones", age: 63 },
+  { id: "7", name: "Elton Smith", age: 16 },
+  { id: "8", name: "Simon Peterson", age: 30 },
+  { id: "9", name: "Daniel Cane", age: 51 },
 ];
+
+const getUserAverageAge =
+  users.reduce((a, b) => {
+    return a + b.age;
+  }, 0) / users.length;
+
+const getUsersNames = users.map((user) => user.name);
+
+console.log(getUserAverageAge);
+
+console.log(getUsersNames);
