33a9bc838675cff95fba97426ea25ef217de2825
andriuszaz <a.zazeckas@gmail.com>
2022-10-29 16:01:38 +0300

    Almost completed 'class constructor' Task-9

--- CA-JS-exam/Task-9/script.js
+++ CA-JS-exam/Task-9/script.js
@@ -9,3 +9,25 @@ budget: number
 Metodas: 
 wasExpensive() - jeigu filmo "budget" yra daugiau nei 100 000 000 mln USD, tada grąžins true, kitu atveju false. 
 ------------------------------------------------------------------------------------------------------ */
+
+class Movie {
+  constructor(title, director, budget) {
+    this.title = title;
+    this.director = director;
+    this.budget = budget;
+  }
+
+  wasExpensive() {
+    const expensiveBudget = 100_000_000;
+
+    if (this.budget >= expensiveBudget) {
+      return true;
+    } else {
+      return;
+    }
+  }
+}
+
+const movie = new Movie("The Matrix", "Wasowski", 100_000_000);
+
+console.log(movie.wasExpensive());
