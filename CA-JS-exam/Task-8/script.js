a5a462fb26e678f87434704a6b4977c9d43e3d9c
andriuszaz <a.zazeckas@gmail.com>
2022-10-29 15:27:33 +0300

    Completed 'calculator constructor' Task-8

--- CA-JS-exam/Task-8/script.js
+++ CA-JS-exam/Task-8/script.js
@@ -6,6 +6,26 @@ multiplication(a, b) - priima du skaičius ir grąžina jų daugybos rezultatą;
 division(a, b) - priima du skaičius ir grąžina jų dalybos rezultatą;
 ------------------------------------------------------------------------------------ */
 
-function Calculator(sum, subtraction, multiplication, division) {
-  this.sum = sum;
+function Calculator(a, b) {
+  this.a = a;
+  this.b = b;
+  this.sum = function () {
+    return +(this.a + this.b);
+  };
+  this.subtraction = function () {
+    return +(this.a - this.b);
+  };
+  this.multiplication = function () {
+    return +(this.a * this.b);
+  };
+  this.division = function () {
+    return +(this.a / this.b);
+  };
 }
+
+const calculate = new Calculator(5, 6);
+
+console.log(calculate.sum());
+console.log(calculate.subtraction());
+console.log(calculate.multiplication());
+console.log(calculate.division());
