8b5f87f2a719a4c3ade73181173795cd3c4c2430
andriuszaz <a.zazeckas@gmail.com>
2023-01-19 21:01:31 +0200

    Added node modules, index and config files

--- CA-NodeJS-MySQL-exam/backend/index.ts
+++ CA-NodeJS-MySQL-exam/backend/index.ts
@@ -1,1 +1,11 @@
 import express from "express";
+import cors from "cors";
+import mysql from "mysql2/promise";
+import { MYSQL_CONFIG } from "./src/config";
+import { PORT } from "./src/config";
+
+const app = express();
+
+app.use(express.json());
+
+app.listen(PORT, () => console.log(`Server is running on port: ${PORT}`));
